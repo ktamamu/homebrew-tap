8cb6d4b6548bf547c13060c0b5f417edd8cce2a2
ktamamu <51480849+ktamamu@users.noreply.github.com>
2025-11-23 17:02:07 +0900

    sglobal: update to v0.1.1

--- Formula/sglobal.rb
+++ Formula/sglobal.rb
@@ -1,36 +1,14 @@
 class Sglobal < Formula
   desc "AWS Security Group Global Access Scanner"
   homepage "https://github.com/ktamamu/sglobal"
-  url "https://github.com/ktamamu/sglobal/archive/v0.1.0.tar.gz"
-  sha256 "4e0c7ad20b50ed7da63db8d6cb4f0a7ef58995e118da939693c7281f4b1c2195"
+  url "https://github.com/ktamamu/sglobal/archive/v0.1.1.tar.gz"
+  sha256 "131947ae15ef41346955dea3dcd5fe18f2a401101a87574d9ae4b140a65f0500"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "."
-  end
-
-  on_macos do
-    if Hardware::CPU.intel?
-      # Intel Mac support
-    end
-    if Hardware::CPU.arm?
-      # Apple Silicon Mac support
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        # Linux x86_64 support
-      end
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        # Linux ARM64 support
-      end
-    end
   end
 
   test do
